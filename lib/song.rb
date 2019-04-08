68bfbf880ef5729e28390c4084b43eb7c32cf064
Gerardo Manzo <programmingmanzo@gmail.com>
2019-04-08 13:34:05 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -44,7 +44,7 @@ class Song
   def self.find_or_create_by_name(name)
     if self.find_by_name(name) == nil
       self.create_by_name(name)
-      binding.pry 
+      #binding.pry 
     else 
       self.create_by_name(name)
     end 
