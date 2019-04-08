08d76a426c20860d0974a5c19826faa6eda838b6
Gerardo Manzo <programmingmanzo@gmail.com>
2019-04-08 12:54:22 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -20,16 +20,16 @@ class Song
   end
   
   def self.new_by_name(name) #This method has a property of name. 
-    song = self.new
-    song.name = name 
-    song
+    new_song = self.new
+    new_song.name = name 
+    new_song
     #binding.pry 
   end 
   
   def self.create_by_name(name)
-    new_song = self.new 
-    new_song.name = name
-    @@all << new_song
+    song = self.new 
+    song.name = name
+    @@all << song
   end 
   
   
