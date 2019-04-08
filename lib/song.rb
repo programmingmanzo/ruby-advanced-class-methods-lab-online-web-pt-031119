e063fdf270e8e4700c082c980f7b754f05bfb17c
Gerardo Manzo <programmingmanzo@gmail.com>
2019-04-08 12:20:22 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -3,11 +3,6 @@ require 'pry'
 class Song
   attr_accessor :name, :artist_name
   @@all = []
-  
-  def initialize(name, artist_name)
-    @name = name 
-    @artist_name = artist_name 
-  end
 
   def self.all
     @@all
@@ -18,8 +13,12 @@ class Song
   end
   
   def self.create
-    @@all 
+    new_song = self.new #When a new song is intantiated it sets it equal to new_song. Then we just return new_song. 
+    new_song 
   end
+  
+  def self.new_by_name(name) #This 
+    
   
   def self.destroy_all 
     self.class.all.clear 
