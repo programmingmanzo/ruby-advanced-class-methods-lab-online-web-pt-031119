20847de006c3f8de6179046aed39055a0b99c666
Gerardo Manzo <programmingmanzo@gmail.com>
2019-04-08 12:49:48 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -14,8 +14,8 @@ class Song
   
   def self.create
     new_song = self.new #When a new song is instantiated it sets it equal to new_song. Then we just return new_song. 
-    @@all << new_song
-    new_song 
+    @@all << new_song #puts all new_songs in the class variable 
+    new_song #returns the new_song 
     #binding.pry 
   end
   
@@ -28,8 +28,7 @@ class Song
   
   def self.create_by_name(name)
     new_song = self.new 
-    new_song.name = name 
-    @all << new_song
+    new_song.name = name
     new_song
   end 
   
